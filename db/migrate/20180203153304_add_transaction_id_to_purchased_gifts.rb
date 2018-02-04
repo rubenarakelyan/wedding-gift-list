3cb469f756cc7726f6eb93b0192e84e20547aa14
Ruben Arakelyan <ruben@arakelyan.uk>
2018-02-04 20:48:25 +0000

    Change GUID to UUID type in migration

--- db/migrate/20180203153304_add_transaction_id_to_purchased_gifts.rb
+++ db/migrate/20180203153304_add_transaction_id_to_purchased_gifts.rb
@@ -1,6 +1,6 @@
 class AddTransactionIdToPurchasedGifts < ActiveRecord::Migration[5.1]
   def change
-    add_column :purchased_gifts, :transaction_id, :guid
+    add_column :purchased_gifts, :transaction_id, :uuid
     add_index :purchased_gifts, :transaction_id
   end
 end
